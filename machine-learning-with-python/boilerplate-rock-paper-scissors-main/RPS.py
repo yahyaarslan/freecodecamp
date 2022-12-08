bf9fc4812b9d554894f3856820a804dd6a870875
yarsel <yarslan02@live.dk>
2022-12-08 12:52:39 +0100

    start

--- machine-learning-with-python/boilerplate-rock-paper-scissors-main/RPS.py
+++
@@ -1,8 +0,0 @@
-my_history = []
-init_play = prev_play = 'S'
-opponent_list = [False, False, False, False]
-ideal_response = {'P' : 'R', 'R' : 'S', 'S' : 'P'}
-opponent_quincy_counter = -1
-play_order = [{
-    RR
-}]
